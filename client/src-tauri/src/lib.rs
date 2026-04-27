c4b804537e057d01d642db6cb91fa0476af412d9
家兴 任 <2216681472@qq.com>
2026-04-28 06:35:55 +0800

    secrets: backup Tauri updater signing keypair (private repo only)

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -11,6 +11,8 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![
             commands::settings::get_settings,
             commands::settings::save_settings,
